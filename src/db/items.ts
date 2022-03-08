22ecba0959c9153f0959a9d2aad254405d8e4ec3
ManoelRIos <manoel.rios@cfc.org.br>
2022-03-08 14:43:54 -0300

    Adicionando dados, data, categoria, valor e descrição

---
+++ src/db/items.ts
@@ -0,0 +1,8 @@
+import {Item} from '../types/Item'
+
+export const items: Item[] = [
+  {date: new Date(2021, 9, 15), category: 'food', title: 'Mc Donalds', value: 32.12 },
+  {date: new Date(2021, 9, 15), category: 'food', title: 'BK', value: 60.20 },
+  {date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel', value: 700 },
+  {date: new Date(2021, 10, 18), category: 'salary', title: 'Salário', value: 1600 }    
+];
