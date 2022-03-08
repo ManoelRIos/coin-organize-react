de4a3634d5e1c890c8fadd173e3d0f371d016925
ManoelRIos <manoel.rios@cfc.org.br>
2022-03-08 15:09:45 -0300

    Adicionando funções de filtragem

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
+import { useState, useEffect } from 'react';
+
+import {getCurrentMonth} from './helpers/dateFilter'
+
 import './App.css'
 
+import {Item} from './types/Item';
+import {Category} from './types/Category';
+
+import {items} from './db/items';
+import {categories} from './db/categories';
+
+
+
 const App = () => {
+
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(() =>{
+    
+  })
+
   return(
     <div className="container">
       <header>
